253d36cb4c828529e3a5703bfea9e601216f01f4
Nina Fadhilah <https://github.com/Ninaaa17/software-engineer-revou-ninaf.git>
2025-02-27 19:19:55 +0700

    second

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// mini project
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Mencegah reload halaman saat submit
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let interest = document.getElementById("interest").value;
+
+    if (name === "" || email === "" || interest === "") {
+        alert("Mohon isi semua bidang formulir!");
+        return;
+    }
+
+    alert("Terima kasih! Kami akan menghubungi Anda segera.");
+});
